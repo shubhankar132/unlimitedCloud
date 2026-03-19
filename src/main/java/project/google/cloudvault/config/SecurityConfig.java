a95ab45155cc63e5d5ee2bdd75cce3a525ff7ea5
Shubhankar Srivastava <shubhankarsrivastava132@gmail.com>
2026-03-19 22:33:33 +0530

    Initial commit

---
+++ src/main/java/project/google/cloudvault/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package project.google.cloudvault.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
---
+++ src/test/java/project/google/cloudvault/CloudvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package project.google.cloudvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
